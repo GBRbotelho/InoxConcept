f65b47abf2c869335e0ef3bc5f9306d0f7ef2ee2
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2024-03-22 16:13:29 -0300

    Container 3

--- src/containers/@Index.jsx
+++ src/containers/@Index.jsx
@@ -3,6 +3,7 @@ import styles from "./@Index.module.css";
 import Navbar from "./Navbar";
 import Container1 from "./Container1";
 import Container2 from "./Container2.jsx";
+import Container3 from "./Container3.jsx";
 
 export default function Index() {
   return (
@@ -10,6 +11,7 @@ export default function Index() {
       <Navbar />
       <Container1 />
       <Container2 />
+      <Container3 />
     </main>
   );
 }
---
+++ src/containers/Container3.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styles from "./Container3.module.css";
+
+export default function Container3() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>
+          Nossos <span>Serviços</span>
+        </h2>
+        <ul>
+          <li>Galpão</li>
+          <li>Galpão</li>
+          <li>Galpão</li>
+          <li>Galpão</li>
+          <li>Galpão</li>
+          <li>Galpão</li>
+        </ul>
+        <div className={styles.content}>
+          <div className={styles.imgs}>
+            <div className={styles.imgs1}>
+              <div className={styles.img1}></div>
+              <div className={styles.img2}></div>
+            </div>
+            <div className={styles.imgs2}>
+              <div className={styles.img3}></div>
+              <div className={styles.dataImg}>
+                <h1>+20</h1>
+                <p>de Experiencia</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.data}>
+            <h2>
+              Galpão
+              <br />
+              industrial
+            </h2>
+            <p>
+              Numa pequena cidade à beira-mar, o tempo parecia parar. As ruas
+              estreitas, adornadas por casas coloridas de estilo colonial,
+              serpenteavam em direção ao oceano, onde as ondas quebravam
+              suavemente na costa.
+              <br />
+              Os habitantes, em sua maioria pescadores e artesãos, viviam em
+              harmonia com a natureza e as tradições que passavam de geração em
+              geração.
+            </p>
+            <div className={styles.icons}>
+              <p>
+                Qualidade
+                <br />
+                garantida
+              </p>
+            </div>
+            <div className={styles.icons}>
+              <p>
+                Pagamento
+                <br />
+                simplificado
+              </p>
+            </div>
+            <div className={styles.icons}>
+              <p>
+                Materias
+                <br />
+                resistentes
+              </p>
+            </div>
+            <button>Fazer orçamento</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
