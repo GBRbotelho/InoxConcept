26d9bf5646e4697ed17331dd12582b86082104e6
GBRbotelho <gabrielbbotelho05@gmail.com>
2024-05-11 21:27:38 -0300

    Maps

--- src/containers/Container5.jsx
+++ src/containers/Container5.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Container5.module.css";
-// import Map from "../icons/Obras/Map";
+import Map from "../icons/Obras/Map";
 import Phone from "../icons/Contato/Phone";
 import Instagram from "../icons/Contato/Instagram";
 
@@ -13,19 +13,20 @@ export default function Container5({ href }) {
         </h1>
         <div className={styles.content} data-aos="zoom-in">
           <div className={styles.data}>
-            {/* <div className={styles.card}>
+            <div className={styles.card}>
               <div className={styles.icon}>
                 <Map />
               </div>
               <div className={styles.dataCard}>
-                <h3>Endereço</h3>
+                <h2>Endereço</h2>
                 <p>
-                  Rua joão marino da silva, 23 campinas sp
+                  R. José Ferreira da Silva, 434 - Quadra LE - Parque da
+                  Figueira
                   <br />
                   cep 12374345
                 </p>
               </div>
-            </div> */}
+            </div>
             <div className={styles.card}>
               <div className={styles.icon}>
                 <Phone />
@@ -49,7 +50,14 @@ export default function Container5({ href }) {
               </div>
             </div>
           </div>
-          {/* <div className={styles.maps}></div> */}
+          <div className={styles.maps}>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14696.72625662531!2d-47.0620964!3d-22.9435395!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa5ecfbc490b48fbd%3A0xb209763aa8ce1ea5!2sINOX%20CONCEITO!5e0!3m2!1spt-BR!2sbr!4v1715473501129!5m2!1spt-BR!2sbr"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
       </div>
     </section>
